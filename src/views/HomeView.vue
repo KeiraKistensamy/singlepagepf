ac7ae37d74c50883b306952e93200414254f5d51
KeiraKistensamy <Keirakist0987@gmail.com>
2024-06-26 11:02:33 +0200

    navbarSetup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,67 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="container">
+<!-- landingSection -->
+    <section class="row" id="landing">
+
+
+
+    </section>
+
+<!-- aboutSection -->
+    <section class="row" id="about">
+
+
+
+    </section>
+
+<!-- skillsSection -->
+    <section class="row" id="skills">
+
+
+
+    </section>
+
+<!-- resumeSection -->
+    <section class="row" id="resume">
+
+
+
+    </section>
+
+<!-- projectsSection -->
+    <section class="row" id="projects">
+
+
+
+    </section>
+
+<!-- testimonialsSection -->
+    <section class="row" id="testimonals">
+
+
+
+    </section>
+
+<!-- reachmeSection -->
+    <section class="row" id="reachme">
+
+
+
+    </section>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
+
+<style>
+
+</style>
